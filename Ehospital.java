3bfffd60462f087386bf0cc651f70934da48ff9c
Mr Reyaz <reyazahmad7045@gmail.com>
2026-01-15 14:20:24 +0530

    Update credits in Ehospital.java

--- Ehospital.java
+++ Ehospital.java
@@ -23,7 +23,7 @@ System.out.print("\t\t\t\t\t|                                                   
 System.out.print("\t\t\t\t\t|                                                                                           |\n");
 System.out.print("\t\t\t\t\t|                                                                                           |\n");
 System.out.print("\t\t\t\t\t|                                         -Brought To You by                                |\n");
-System.out.print("\t\t\t\t\t|                                      	 Md Fahad,Omer Mohiuddin and Yaseen Hussain         |\n");
+System.out.print("\t\t\t\t\t|                                      	 MR Reyaz, 									        |\n");
 System.out.print("\t\t\t\t\t|___________________________________________________________________________________________|\n");
 }
 //Declaring variales to be used
@@ -348,7 +348,7 @@ System.out.print("\t\t\t\t\t|                                                   
 System.out.print("\t\t\t\t\t|                                                                                       |\n");
 System.out.print("\t\t\t\t\t|                                                                                       |\n");
 System.out.print("\t\t\t\t\t|                                                                                       |\n");
-System.out.print("\t\t\t\t\t|                                              -Brought To You by Fahad,Omer and Yaseen |\n");
+System.out.print("\t\t\t\t\t|                                              -Brought To You by Reyaz,sajid and parwaj |\n");
 System.out.print("\t\t\t\t\t|_______________________________________________________________________________________|\n");
 System.out.print("\t\t\t\t\t\n\n\n\n\t\t\t\t\t");
 System.exit(0);
